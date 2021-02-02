4bd3a922d5efe05ead6961299d497d767c9871fc
SungYeolha <tjddufgk@gmail.com>
2021-02-02 18:12:18 +0900

    Styled Components & Global Styles & README.md

---
+++ src/global-styles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const globalStyles = createGlobalStyle`
+    ${reset}
+    a {
+        text-decoration:none;
+        color:inherit;
+    }
+    * {
+        box-sizing : border-box
+    }body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size:12px;
+        background-color:rgba(20, 20, 20, 1);
+        color:white;
+    }
+    button { 
+        cursor: pointer;
+        border: none;
+        outline: none;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
