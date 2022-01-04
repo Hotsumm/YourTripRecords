e8a14cd624deaf30785705ccfd2c4bef938b76a8
SungYeolha <tjddufgk@gmail.com>
2022-01-04 22:35:13 +0900

    feat: add CityPostLink wrapper

--- src/components/City/CityPost.jsx
+++ src/components/City/CityPost.jsx
@@ -5,71 +5,46 @@ import { RiHeartFill } from 'react-icons/ri';
 import Loading from '../Load/Loading';
 import noPostImg from '../../static/assets/NoPost1.jpeg';
 
-const CityPostContainer = styled.div`
-  @media (max-width: 1024px) {
-    padding: 0 30px 40px 30px;
-  }
+const CityPostContainer = styled.section`
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 0px 40px 40px 40px;
 `;
 
-const NoPostWrap = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const NoPostImgWrap = styled.div`
-  @media (max-width: 768px) {
-    width: 100%;
-    height: 400px;
-  }
-  @media (max-width: 576px) {
-    width: 100%;
-    height: 250px;
-  }
-  @media (max-width: 320px) {
-    width: 100%;
-    height: 150px;
-  }
-  width: 768px;
-  height: 500px;
-  border-radius: 10px;
-  & img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const NoPost = styled.p`
-  white-space: pre;
-  font-size: 4vw;
-  margin-bottom: 10px;
-  color: black;
-`;
-
-const CityPostWrap = styled.div`
+const CityPostWrap = styled.ul`
   @media (max-width: 1200px) {
     grid-template-columns: repeat(3, 1fr);
   }
-  @media (max-width: 992px) {
+  @media (max-width: 1000px) {
     grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+    padding: 0 20px;
   }
   @media (max-width: 576px) {
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 0 10px;
     width: 100%;
   }
   width: 100%;
   display: grid;
   gap: 50px 30px;
+  padding: 0 40px;
   grid-template-columns: repeat(4, 1fr);
+  margin-bottom: 40px;
+`;
+
+const CityPostLinkWrap = styled.li`
+  width: 100%;
+  cursor: pointer;
+  border: 1px solid #16a085;
+  border-radius: 5px;
+  &:hover {
+    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.4);
+  }
 `;
 
 const CityPostLink = styled(Link)`
@@ -78,19 +53,13 @@ const CityPostLink = styled(Link)`
 
 const PostIntro = styled.div`
   position: relative;
+  width: 100%;
   @media (max-width: 500px) {
-    width: 100%;
     align-items: center;
   }
   height: 100%;
   display: flex;
   flex-direction: column;
-  border: 1px solid #16a085;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.4);
-  }
 `;
 
 const PostThumbnail = styled.div`
@@ -185,17 +154,17 @@ const CreatorWrap = styled.div`
   display: flex;
   align-items: center;
 `;
+
 const InfoWrap = styled.span`
   display: flex;
   flex-direction: column;
-`;
-
-const Info = styled.span`
-  font-size: 12px;
-  :first-child {
-    margin-top: 5px;
+  & span {
+    font-size: 12px;
+    :first-child {
+      margin-top: 5px;
+    }
+    margin-bottom: 5px;
   }
-  margin-bottom: 5px;
 `;
 
 const Avatar = styled.img`
@@ -210,6 +179,51 @@ const Nickname = styled.span`
   font-weight: 700;
 `;
 
+// NoPost
+const NoPostWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+const NoPostImgWrap = styled.div`
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 400px;
+  }
+  @media (max-width: 576px) {
+    width: 100%;
+    height: 250px;
+  }
+  @media (max-width: 320px) {
+    width: 100%;
+    height: 150px;
+  }
+  width: 768px;
+  height: 500px;
+  border-radius: 10px;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const NoPost = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  & p {
+    white-space: pre;
+    line-height: 150%;
+    text-align: center;
+    font-size: 2rem;
+    color: black;
+  }
+`;
+
 const CityPost = ({ loading, posts }) => {
   return (
     <CityPostContainer>
@@ -220,64 +234,70 @@ const CityPost = ({ loading, posts }) => {
           {posts && posts.length > 0 ? (
             <CityPostWrap>
               {posts.map((post) => (
-                <CityPostLink
-                  key={post.postId}
-                  to={{
-                    pathname: `/city/${post.city}/${post.postId}`,
-                    state: { cityName: post.city, post: post },
-                  }}
-                >
-                  <PostIntro>
-                    <PostThumbnail>
-                      <img
-                        src={post.pictureList[0].pictureURL}
-                        alt={post.postId}
-                      />
-                      <PictureCountWrap>
-                        <span>+{post.pictureList.length - 1}</span>
-                      </PictureCountWrap>
-                    </PostThumbnail>
-                    {post.hashtags && (
-                      <HashtagWrap>
-                        {post.hashtags.map((hashtag, index) => (
-                          <Hashtag key={index}>{hashtag}</Hashtag>
-                        ))}
-                      </HashtagWrap>
-                    )}
-                    <PostInfo>
-                      <PostCountWrap>
-                        <RiHeartFill size={'16'} style={{ color: '#ff4757' }} />
-                        <LikeCount>{post.likes.length}</LikeCount>
-                        <CommentCount>({post.comments.length})</CommentCount>
-                      </PostCountWrap>
-                      <PostTitle>
-                        {post.postTitle.length > 15
-                          ? `${post.postTitle.substring(0, 15)}...`
-                          : post.postTitle}
-                      </PostTitle>
-                      <InfoContent>
-                        <InfoWrap>
-                          <Info>도시: {post.city}</Info>
-                          <Info>여행계절: {post.season}</Info>
-                        </InfoWrap>
-                        <CreatorWrap>
-                          <Avatar
-                            src={post.creator.userObj.avatar}
-                            alt="Avatar"
+                <CityPostLinkWrap key={post.postId}>
+                  <CityPostLink
+                    to={{
+                      pathname: `/city/${post.city}/${post.postId}`,
+                      state: { cityName: post.city, post: post },
+                    }}
+                  >
+                    <PostIntro>
+                      <PostThumbnail>
+                        <img
+                          src={post.pictureList[0].pictureURL}
+                          alt="썸네일"
+                        />
+                        <PictureCountWrap>
+                          <span>+{post.pictureList.length - 1}</span>
+                        </PictureCountWrap>
+                      </PostThumbnail>
+                      {post.hashtags && (
+                        <HashtagWrap>
+                          {post.hashtags.map((hashtag, index) => (
+                            <Hashtag key={index}>{hashtag}</Hashtag>
+                          ))}
+                        </HashtagWrap>
+                      )}
+                      <PostInfo>
+                        <PostCountWrap>
+                          <RiHeartFill
+                            size={'16'}
+                            style={{ color: '#ff4757' }}
                           />
-                          <Nickname>{post.creator.userObj.nickname}</Nickname>
-                        </CreatorWrap>
-                      </InfoContent>
-                    </PostInfo>
-                  </PostIntro>
-                </CityPostLink>
+                          <LikeCount>{post.likes.length}</LikeCount>
+                          <CommentCount>({post.comments.length})</CommentCount>
+                        </PostCountWrap>
+                        <PostTitle>
+                          {post.postTitle.length > 15
+                            ? `${post.postTitle.substring(0, 15)}...`
+                            : post.postTitle}
+                        </PostTitle>
+                        <InfoContent>
+                          <InfoWrap>
+                            <span>도시: {post.city}</span>
+                            <span>여행계절: {post.season}</span>
+                          </InfoWrap>
+                          <CreatorWrap>
+                            <Avatar
+                              src={post.creator.userObj.avatar}
+                              alt="프로필 사진"
+                            />
+                            <Nickname>{post.creator.userObj.nickname}</Nickname>
+                          </CreatorWrap>
+                        </InfoContent>
+                      </PostInfo>
+                    </PostIntro>
+                  </CityPostLink>
+                </CityPostLinkWrap>
               ))}
             </CityPostWrap>
           ) : (
             <NoPostWrap>
-              <NoPost>아직 등록 된 게시물이 없습니다.</NoPost>
-              <NoPost style={{ marginBottom: '40px' }}>
-                첫 게시물을 등록 해보세요 !
+              <NoPost>
+                <p>
+                  아직 등록 된 게시물이 없습니다. <br />첫 게시물을
+                  등록해보세요!
+                </p>
               </NoPost>
               <NoPostImgWrap>
                 <img src={noPostImg} alt="게시물 없음" />
