636c7d2108cff06ebb5a2bf60a988950028158f0
SungYeolha <tjddufgk@gmail.com>
2022-01-18 22:43:11 +0900

    chore: add @types dir

---
+++ src/@types/types.d.ts
@@ -0,0 +1,50 @@
+// USER
+
+interface IUserObj {
+  userId: string;
+  avatar: string;
+  createdAt: string;
+  email: string;
+  intro?: string;
+  isSocial: boolean;
+  nickname: string;
+  records: string[];
+}
+
+interface IUserContext {
+  userObj: IUserObj | null;
+  refreshUser?: (sign: boolean) => void;
+}
+
+// POST
+interface IPost {
+  postId: string;
+  city: string;
+  postTitle: string;
+  season: string;
+  createdAt: string;
+  hashtags: string[];
+  likes: string[];
+  comments: string[];
+  creator: { userObj: IUserObj };
+  pictureList: IPictureList[];
+}
+
+interface IPictureList {
+  pictureId: string;
+  location: ILocation | null;
+  description: string;
+  pictureURL: string;
+  fileName: string;
+}
+
+interface ILocation {
+  coords: ICoords;
+  locationId: string;
+  placeName: string;
+}
+
+interface ICoords {
+  latitude: string;
+  longitude: string;
+}
