d00a727d78e962b558250e2f7e7a9d2fc8bb0078
Sungyeolha <tjddufgk@gmail.com>
2022-01-24 01:35:55 +0900

    chore: add user type
    * chore: add CreateUser type
    
    * chore: add CreateSocialUser type

--- src/components/User/CreateSocialUser.ts
+++ src/components/User/CreateSocialUser.ts
@@ -2,15 +2,25 @@ import { firebaseFireStore } from '../../firebaseConfig';
 import { getCreatedDay } from '../../utils/getCreatedDay';
 import { firebaseAuth } from '../../firebaseConfig';
 
-export const CreateUser = async (email, nickname) => {
-  const currentUser = firebaseAuth.currentUser;
+interface CreateSocialUserParam {
+  (email: string, nickname: string, avatar: string): Promise<void>;
+}
+
+export const CreateSocialUser: CreateSocialUserParam = async (
+  email,
+  nickname,
+  avatar,
+) => {
+  const currentUser: any = firebaseAuth.currentUser;
   const defaultAvatar =
     'https://firebasestorage.googleapis.com/v0/b/travel-7a141.appspot.com/o/UserProfle%2FU3NaFKaoyGYnYozURq4p2XHsqkw2%2FdefaultAvatar.png?alt=media&token=dc3a629e-1934-4db6-abf0-e918c306d004';
+
   const docData = {
+    isSocial: true,
     userId: currentUser.uid,
     email,
     nickname,
-    avatar: defaultAvatar,
+    avatar: avatar ? avatar : defaultAvatar,
     records: [],
     createdAt: getCreatedDay(),
   };
@@ -18,5 +28,5 @@ export const CreateUser = async (email, nickname) => {
     .collection('users')
     .doc(currentUser.uid)
     .set(docData)
-    .catch((error) => console.log(error));
+    .catch((error) => alert(error));
 };
--- src/components/User/CreateUser.ts
+++ src/components/User/CreateUser.ts
@@ -2,17 +2,19 @@ import { firebaseFireStore } from '../../firebaseConfig';
 import { getCreatedDay } from '../../utils/getCreatedDay';
 import { firebaseAuth } from '../../firebaseConfig';
 
-export const CreateSocialUser = async (email, nickname, avatar) => {
-  const currentUser = firebaseAuth.currentUser;
+interface CreateUserParam {
+  (email: string, nickname: string): Promise<void>;
+}
+
+export const CreateUser: CreateUserParam = async (email, nickname) => {
+  const currentUser: any = firebaseAuth.currentUser;
   const defaultAvatar =
     'https://firebasestorage.googleapis.com/v0/b/travel-7a141.appspot.com/o/UserProfle%2FU3NaFKaoyGYnYozURq4p2XHsqkw2%2FdefaultAvatar.png?alt=media&token=dc3a629e-1934-4db6-abf0-e918c306d004';
-
   const docData = {
-    isSocial: true,
     userId: currentUser.uid,
     email,
     nickname,
-    avatar: avatar ? avatar : defaultAvatar,
+    avatar: defaultAvatar,
     records: [],
     createdAt: getCreatedDay(),
   };
@@ -20,5 +22,5 @@ export const CreateSocialUser = async (email, nickname, avatar) => {
     .collection('users')
     .doc(currentUser.uid)
     .set(docData)
-    .catch((error) => alert(error));
+    .catch((error) => console.log(error));
 };
