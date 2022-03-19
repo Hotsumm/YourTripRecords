1f599ae1375e3dd175bbb5496744e964c0fe655d
SungYeolha <tjddufgk@gmail.com>
2022-03-20 02:46:26 +0900

    chore: change environment variable

---
+++ src/@types/env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMeta {
+  env: {
+    VITE_API_KEY: string;
+    VITE_APP_ID: string;
+    VITE_AUTH_DOMAIN: string;
+    VITE_MEASUREMENT_ID: string;
+    VITE_MESSAGINGSENDER_ID: string;
+    VITE_PROJECT_ID: string;
+    VITE_STORAGE_BUCKET: string;
+  };
+}
