2fa57a4393883b5faaf8d0077e86ff3abb039991
SungYeolha <tjddufgk@gmail.com>
2022-04-12 00:54:40 +0900

    feat: add ReactDOMClient

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const container: HTMLElement | null = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
-hydrateRoot(
+const root = ReactDOMClient.hydrateRoot(
   container,
   <React.StrictMode>
     <BrowserRouter>
