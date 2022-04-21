03be82effbf8d3fc6116456d1ef554ff8a32bc0f
SungYeolha <tjddufgk@gmail.com>
2022-04-22 01:42:29 +0900

    add vite config path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
-
+import path from 'path';
 export default defineConfig({
   server: {
     port: 3000,
@@ -9,6 +9,18 @@ export default defineConfig({
   build: {
     minify: false,
     sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@static': path.resolve(__dirname, './src/static'),
+      '@assets': path.resolve(__dirname, './src/static/assets'),
+    },
   },
   plugins: [
     react({
