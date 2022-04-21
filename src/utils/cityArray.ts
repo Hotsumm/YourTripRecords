1db9f202c1aaf78045fbf84c4585cce8b5708514
SungYeolha <tjddufgk@gmail.com>
2022-04-22 01:37:17 +0900

    refactor: cityArray

--- src/utils/cityArray.ts
+++ src/utils/cityArray.ts
@@ -1,54 +1,19 @@
-export const cityArray: ICityArray[] = [
-  {
-    id: 1,
-    name: '서울',
-  },
-  {
-    id: 2,
-    name: '경기',
-  },
-  {
-    id: 3,
-    name: '인천',
-  },
-  {
-    id: 4,
-    name: '울산',
-  },
-  {
-    id: 5,
-    name: '강원',
-  },
-  {
-    id: 6,
-    name: '충청',
-  },
-  {
-    id: 7,
-    name: '대전',
-  },
-  {
-    id: 8,
-    name: '세종',
-  },
-  {
-    id: 9,
-    name: '전라',
-  },
-  {
-    id: 10,
-    name: '광주',
-  },
-  {
-    id: 11,
-    name: '대구',
-  },
-  {
-    id: 12,
-    name: '부산',
-  },
-  {
-    id: 13,
-    name: '제주',
-  },
+const cityList = [
+  '서울',
+  '경기',
+  '인천',
+  '울산',
+  '강원',
+  '충청',
+  '대전',
+  '세종',
+  '전라',
+  '광주',
+  '대구',
+  '부산',
+  '제주',
 ];
+
+export const cityArray: ICityArray[] = cityList.map((city, index) => {
+  return { id: index, name: city };
+});
